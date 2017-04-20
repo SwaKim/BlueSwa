119221fec76d51fdd07a3aeed226bdeed78c9dfa
DESKTOP-C0N9JKG\kkk32 <kkk3206@naver.com>
2017-04-20 15:13:06 +0900

    질문글 수정 및 삭제 기능 1차구현

--- src/main/java/com/Bluswa/tistory/web/QuestionController.java
+++ src/main/java/com/Bluswa/tistory/web/QuestionController.java
@@ -4,8 +4,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.Bluswa.tistory.domain.Question;
@@ -38,5 +42,30 @@ public class QuestionController {
 		questionRepository.save(newQuestion);				
 		return "redirect:/";
 	}
-
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		model.addAttribute("question", questionRepository.findOne(id));
+		return "/qna/show"; 
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		model.addAttribute("question", questionRepository.findOne(id));
+		return "/qna/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, String title, String contents) {
+		Question question = questionRepository.findOne(id);
+		question.update(title, contents); // question.update가 없을때, ctrl + 1 메소드 생성, question.java에 자동으로 틀 만들어짐
+		questionRepository.save(question);
+		return String.format("redirect:/questions/%d", id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable Long id) {
+		questionRepository.delete(id);
+		return "redirect:/";
+	}
 }
